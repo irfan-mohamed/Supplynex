6c5f47fe76c40abdc51667bdb913e6482b1ad46d
irfan-mohamed <irfanmohamedvdk@gmail.com>
2026-06-01 11:11:50 +0530

    fix: Ui - Mobile view brand logo Resize

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -1,6 +1,5 @@
 import darkLogo from "../Images/logo_white.PNG";
 import lightLogo from "../Images/logo_dark.PNG";
-import mobileLogo from "../Images/logo_white.PNG";
 
 type BrandLogoProps = {
   variant?: "dark" | "light";
@@ -10,7 +9,6 @@ type BrandLogoProps = {
 export function BrandLogo({ variant = "dark", className = "" }: BrandLogoProps) {
   return (
     <picture className={`brand-logo-wrap ${className}`}>
-      {variant === "dark" ? <source media="(max-width: 560px)" srcSet={mobileLogo} /> : null}
       <img
         className="brand-logo"
         src={variant === "dark" ? darkLogo : lightLogo}
